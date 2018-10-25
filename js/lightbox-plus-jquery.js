567027ed0736341f7b1e6a761edb3033c23f5b3c
stahusta <marcin.sasin1@gmail.com>
2018-10-25 23:35:39 +0200

    nowe

--- js/lightbox-plus-jquery.js
+++ js/lightbox-plus-jquery.js
@@ -10306,7 +10306,7 @@ return jQuery;
     resizeDuration: 700,
     showImageNumberLabel: false,
     wrapAround: false,
-    disableScrolling: true,
+    disableScrolling: false,
     /*
     Sanitize Title
     If the caption data is trusted, for example you are hardcoding it in, then leave this to false.
